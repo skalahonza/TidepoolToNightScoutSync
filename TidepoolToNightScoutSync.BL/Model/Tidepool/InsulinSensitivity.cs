4eb8141c51162c0a7c23e0ebc59f847cb124ab87
Jan Skála <j.skala@hotmail.com>
2021-07-24 19:01:31 +0200

    Map insulin sensitivities.

---
+++ TidepoolToNightScoutSync.BL/Model/Tidepool/InsulinSensitivity.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace TidepoolToNightScoutSync.BL.Model.Tidepool
+{
+    public class InsulinSensitivity
+    {
+        [JsonProperty("amount")]
+        public double Amount { get; set; }
+
+        [JsonProperty("start")]
+        public int Start { get; set; }
+    }
+}
