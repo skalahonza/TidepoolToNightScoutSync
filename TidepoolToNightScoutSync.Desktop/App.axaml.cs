41f1e8fa12c0291afb04f554898136b1ca7f2c14
Jan Skála <j.skala@hotmail.com>
2022-05-08 11:50:14 +0200

    Can sync with button.

--- TidepoolToNightScoutSync.Desktop/App.axaml.cs
+++ TidepoolToNightScoutSync.Desktop/App.axaml.cs
@@ -1,6 +1,11 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using TidepoolToNightScoutSync.BL.Extensions;
 using TidepoolToNightScoutSync.Desktop.ViewModels;
 using TidepoolToNightScoutSync.Desktop.Views;
 
@@ -15,15 +20,47 @@ namespace TidepoolToNightScoutSync.Desktop
 
         public override void OnFrameworkInitializationCompleted()
         {
+            var configuration = CreateConfiguration();
+            var services = ConfigureServices(configuration);
+            var provider = services.BuildServiceProvider();
+
             if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
             {
                 desktop.MainWindow = new MainWindow
                 {
-                    DataContext = new MainWindowViewModel(),
+                    DataContext = provider.GetRequiredService<MainWindowViewModel>()
                 };
             }
 
             base.OnFrameworkInitializationCompleted();
         }
+
+        private static IServiceCollection ConfigureServices(IConfiguration configuration)
+        {
+            //logging
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .Enrich.With<Sanitizer>()
+                .CreateLogger();
+
+            var services = new ServiceCollection();
+
+            services.AddTransient<MainWindowViewModel>();
+            services.AddSingleton(configuration)
+                .AddTidepoolClient((settings, c) => c.GetSection("tidepool").Bind(settings))
+                .AddNightscoutClient((settings, c) => c.GetSection("nightscout").Bind(settings))
+                .AddTidepoolToNightScoutSyncer((settings, c) => c.GetSection("sync").Bind(settings))
+                .AddLogging(x => x.AddSerilog());
+
+            return services;
+        }
+
+        private static IConfiguration CreateConfiguration() =>
+            new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile("credentials.json", true, true)
+                .AddEnvironmentVariables()
+                .AddUserSecrets<Program>()
+                .Build();
     }
 }
--- TidepoolToNightScoutSync.Desktop/ViewModels/MainWindowViewModel.cs
+++ TidepoolToNightScoutSync.Desktop/ViewModels/MainWindowViewModel.cs
@@ -3,12 +3,26 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using ReactiveUI;
+using TidepoolToNightScoutSync.BL.Services;
 
 namespace TidepoolToNightScoutSync.Desktop.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private readonly TidepoolToNightScoutSyncer _syncer;
+        
         private bool _isRunning;
+
+        public MainWindowViewModel()
+        {
+            _syncer = null!;
+        }
+
+        public MainWindowViewModel(TidepoolToNightScoutSyncer syncer)
+        {
+            _syncer = syncer;
+        }
+
         public string AppName => "Tidepool to NightScout sync tool";
 
         public bool IsRunning
@@ -17,10 +31,13 @@ namespace TidepoolToNightScoutSync.Desktop.ViewModels
             set => _isRunning = this.RaiseAndSetIfChanged(ref _isRunning, value);
         }
 
+        public DateTimeOffset Since { get; set; } = DateTime.Today.AddDays(-7);
+
         public async void RunSyncNow()
         {
             IsRunning = true;
-            await Task.Delay(TimeSpan.FromSeconds(2));
+            await _syncer.SyncProfiles(Since.UtcDateTime);
+            await _syncer.SyncAsync(Since.UtcDateTime);
             IsRunning = false;
         }
     }
