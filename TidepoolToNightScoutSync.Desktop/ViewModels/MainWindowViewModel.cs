d942a783264df48407a4df8e98dac52079aa1226
Jan Skála <j.skala@hotmail.com>
2022-05-08 09:04:19 +0200

    Add sync animation.

--- TidepoolToNightScoutSync.Desktop/ViewModels/MainWindowViewModel.cs
+++ TidepoolToNightScoutSync.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using ReactiveUI;
 
 namespace TidepoolToNightScoutSync.Desktop.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
+        private bool _isRunning;
         public string AppName => "Tidepool to NightScout sync tool";
+
+        public bool IsRunning
+        {
+            get => _isRunning;
+            set => _isRunning = this.RaiseAndSetIfChanged(ref _isRunning, value);
+        }
+
+        public async void RunSyncNow()
+        {
+            IsRunning = true;
+            await Task.Delay(TimeSpan.FromSeconds(2));
+            IsRunning = false;
+        }
     }
 }
